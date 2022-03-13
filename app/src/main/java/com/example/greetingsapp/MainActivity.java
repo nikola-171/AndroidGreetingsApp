7bc529b1b822550416601b3e25f4fc42afb1734b
Nikola Tošić <nikola.tosic98@outlook.com>
2022-03-13 17:02:14 +0100

    App finished, added main functionality

--- app/src/main/java/com/example/greetingsapp/MainActivity.java
+++ app/src/main/java/com/example/greetingsapp/MainActivity.java
@@ -6,22 +6,33 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private Button _nameInputActivityButton = null;
+    private Button _greetingButton = null;
+
+    private String _firstName = "";
+    private String _lastName = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        _firstName = getIntent().getStringExtra("FirstName");
+        _lastName = getIntent().getStringExtra("LastName");
+
         _nameInputActivityButton = findViewById(R.id.button_name);
-        _nameInputActivityButton.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View view) {
-                OpenNameInputActivity();
-            }
-        });
+        _greetingButton = findViewById(R.id.button_greeting);
+
+        if(_firstName != null && _lastName != null){
+            _greetingButton.setEnabled(!_firstName.isEmpty() && !_lastName.isEmpty());
+        }
+
+        _nameInputActivityButton.setOnClickListener(view -> OpenNameInputActivity());
+
+        _greetingButton.setOnClickListener(view -> DisplayGreetingMessage());
 
     }
 
@@ -29,5 +40,10 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this, NameInput.class);
 
         startActivity(intent);
+    }
+
+    private void DisplayGreetingMessage(){
+        Toast.makeText(MainActivity.this,
+                String.format("Greetings to you %s %s", _firstName, _lastName), Toast.LENGTH_LONG).show();
     }
 }
--- app/src/main/java/com/example/greetingsapp/NameInput.java
+++ app/src/main/java/com/example/greetingsapp/NameInput.java
@@ -4,13 +4,21 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+
+import org.w3c.dom.Text;
 
 public class NameInput extends AppCompatActivity {
 
     private Button _buttonCancel = null;
     private Button _buttonSave = null;
+
+    private EditText _firstNameInput = null;
+    private EditText _lastNameInput = null;
 
     private String _firstName = "";
     private String _lastName = "";
@@ -20,20 +28,49 @@ public class NameInput extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_name_input);
 
-        _buttonCancel = findViewById(R.id.button_cancel);
-        _buttonCancel.setOnClickListener(new View.OnClickListener(){
+        _firstNameInput = findViewById(R.id.input_first_name);
+        _lastNameInput = findViewById(R.id.input_last_name);
 
-            @Override
-            public void onClick(View view) {
-                GoToMainPage();
-            }
-        });
+        _firstNameInput.addTextChangedListener(textWatcher);
+        _lastNameInput.addTextChangedListener(textWatcher);
+
+
+        _buttonCancel = findViewById(R.id.button_cancel);
+        _buttonCancel.setOnClickListener(view -> GoToMainPage());
         _buttonSave = findViewById(R.id.button_save);
+
+        _buttonSave.setOnClickListener(view -> GoToMainPage());
 
     }
 
+    private TextWatcher textWatcher = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            String firstName = _firstNameInput.getText().toString().trim();
+            String lastName = _lastNameInput.getText().toString().trim();
+
+            _buttonSave.setEnabled(!firstName.isEmpty() && !lastName.isEmpty());
+
+            _firstName = firstName;
+            _lastName = lastName;
+        }
+
+        @Override
+        public void afterTextChanged(Editable editable) {
+
+        }
+    };
+
     private void GoToMainPage(){
         Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra("FirstName", this._firstName);
+        intent.putExtra("LastName", this._lastName);
+
         startActivity(intent);
     }
 }
